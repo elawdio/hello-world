3dbb2ebff574b0d2589ce652ec01266dcb04a9f1
elawdio <eladlevy5@gmail.com>
2018-04-26 16:27:35 +0300

    First Commit!

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets go !");
+            var r = LetsGo(10, 5);
+            for (int i = 0; 4 < 8; i++)
+            {
+                System.Console.WriteLine("");
+            }
+        }
+
+        static int LetsGo(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
